e80dd372b40d23d33023713d782e19d83ad79c7f
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2025-01-12 00:27:57 +0530

    Update about incomplete

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -17,6 +19,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSlideToggleModule,
     MatListModule,
     MatSidenavModule,
+    MatChipsModule,
+    MatProgressBarModule,
   ],
   exports: [
     MatToolbarModule,
