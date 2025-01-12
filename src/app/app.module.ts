405b1c22bcba286557e9ad6c8ed27f67880415fa
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2025-01-12 15:58:27 +0530

    add base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { APP_BASE_HREF } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   CUSTOM_ELEMENTS_SCHEMA,
@@ -11,24 +11,24 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { InMemoryCache } from '@apollo/client/cache';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/navigation/header/header.component';
 import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
+import { HeatmapComponent } from './components/profiles/leetcode/heatmap/heatmap.component';
+import { LeetcodeComponent } from './components/profiles/leetcode/leetcode.component';
+import { UserProfileComponent } from './components/profiles/leetcode/user-profile/user-profile.component';
+import { UserStatisticsComponent } from './components/profiles/leetcode/user-statistics/user-statistics.component';
 import { ProfilesComponent } from './components/profiles/profiles.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { PublicationsComponent } from './components/publications/publications.component';
 import { MaterialModule } from './modules/material.module';
-import { HomeComponent } from './components/home/home.component';
-import { LeetcodeComponent } from './components/profiles/leetcode/leetcode.component';
-import { HeatmapComponent } from './components/profiles/leetcode/heatmap/heatmap.component';
-import { UserProfileComponent } from './components/profiles/leetcode/user-profile/user-profile.component';
-import { UserStatisticsComponent } from './components/profiles/leetcode/user-statistics/user-statistics.component';
 import { ThemeService } from './services/theme.service';
 
 @NgModule({
@@ -62,7 +62,7 @@ import { ThemeService } from './services/theme.service';
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
-    // { provide: APP_BASE_HREF, useValue: '/portfolio' },
+    { provide: APP_BASE_HREF, useValue: '/portfolio' },
     provideApollo(() => {
       const httpLink = inject(HttpLink);
       return {
