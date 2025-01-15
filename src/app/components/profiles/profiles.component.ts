cc15ddc19287add1667f45030931b1bab39a547c
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2025-01-16 01:07:26 +0530

    add leetcode profile

--- src/app/components/profiles/profiles.component.ts
+++ src/app/components/profiles/profiles.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profiles',
   standalone: false,
-  
+
   templateUrl: './profiles.component.html',
-  styleUrl: './profiles.component.scss'
+  styleUrl: './profiles.component.scss',
 })
 export class ProfilesComponent {
-
+  leetcodeAvatar = './image/leetcode.png';
 }
