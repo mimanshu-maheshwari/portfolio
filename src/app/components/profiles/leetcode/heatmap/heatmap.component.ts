4bc961386197a7d8b7f40e863a5477c6750a4a59
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2025-01-18 17:28:37 +0530

    remove heatmap data

--- src/app/components/profiles/leetcode/heatmap/heatmap.component.ts
+++ src/app/components/profiles/leetcode/heatmap/heatmap.component.ts
@@ -1,5 +1,11 @@
-import { Component, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import CalHeatMap from 'cal-heatmap';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-heatmap',
@@ -8,56 +14,7 @@ import CalHeatMap from 'cal-heatmap';
   styleUrl: './heatmap.component.scss',
 })
 export class HeatmapComponent {
-  @ViewChild('heatmapContainer', { static: true }) heatmapContainer!: ElementRef;
+  @ViewChild('heatmapContainer', { static: true })
+  heatmapContainer!: ElementRef;
   @Input() submissionCalendar!: Array<{ date: Date; count: number }>;
-
-  private cal: CalHeatMap; // Declare cal as an instance of the CalHeatMap class
-
-  constructor() {
-    this.cal = new CalHeatMap(); // Instantiate CalHeatMap
-  }
-
-  ngAfterViewInit(): void {
-    if (!this.submissionCalendar) {
-      return;
-    }
-    this.initializeHeatmap();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (!this.submissionCalendar) {
-      return;
-    }
-    this.updateHeatmapData();
-  }
-
-  private initializeHeatmap(): void {
-    if (!this.submissionCalendar) {
-      return;
-    }
-    // Initialize CalHeatMap
-    this.cal.init({
-      itemSelector: '#cal-heatmap',
-      domain: 'month', // You can customize this as per your needs
-      range: 3, // Adjust range if needed
-      data: [], // Placeholder for initial empty data
-      cellSize: 20, // Size of each cell
-    });
-  }
-
-  private updateHeatmapData(): void {
-    if (!this.submissionCalendar) {
-      return;
-    }
-    // Transform the input data to the format Cal-Heatmap expects
-    const formattedData = this.submissionCalendar.map((item) => ({
-      date: new Date(item.date).getTime(), // Convert date to timestamp
-      value: item.count, // Use count as the value
-    }));
-
-    // Update CalHeatMap with new data
-    this.cal.update({
-      data: formattedData,
-    });
-  }
 }
